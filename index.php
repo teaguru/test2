3ed840dfe6a6ea1a6bcec04cfcb105678958335e
teaguru <teaguru@bk.ru>
2019-09-03 14:43:18 +0400

    initial

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$array = [1, 1, 2, 3, 4, -51, 12, 12, 12, -51];
+
+function getPair($array)
+{
+    $pairCount = 0;
+    for ($i = 1; $i <= count($array) - 1; $i++) {
+        if ($array[$i] == $array[$i - 1]) {
+            $pairCount++;
+            echo $i;
+        }
+    }
+    echo $pairCount;
+}
+
+getPair($array);
